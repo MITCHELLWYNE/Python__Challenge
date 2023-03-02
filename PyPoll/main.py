4c5c4edca5b325faba0028c2b3b1cf6dfac04548
MichaelMitch <mitchellwyne@gmail.com>
2023-03-01 17:08:41 -0800

    Adjustment to PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,13 +2,55 @@ import csv
 
 infile="resources/election_data.csv"
 
+#name all variables
 
-Candidate=[]
+votes=0
+votes_next=0
+votes_after=0
+previous_vote=0
+ballot_count=0
+candidate=str
+next_candidate=str
+after_candidate=str
+previous_candidate=str
+change_vote=0
+county=str
+previous_county=str
+current_county=str
+
+with open(infile) as inputpydata:
+    rows=csv.reader(inputpydata)
+    header=next(rows)
 
 
 
+#for loop for how many votes were counted
+    for row in rows:
+        ballot_count=ballot_count+1
+        
 
 
+ #for loop for each county
+ # count how many times each candidate appears in the data set
+ # run for loop to find each candidate   
+    
+    #ballots (row[0])  
+    #county (row[1])
+    #candidate(row[2])
+        current_county=str(row[1])
+        if previous_county!=current_county:
+                    
+        
+
+            
+#send to the top of if statement
+        
+        
+        
+
+
+
+    print(votes) 
         
 
         
@@ -21,7 +63,7 @@ Candidate=[]
 output=f"""
 Election Results
 -------------------------
-Total Votes: 369711
+Total Votes: {ballot_count}
 -------------------------
 Charles Casper Stockham: 23.049% (85213)
 Diana DeGette: 73.812% (272892)
